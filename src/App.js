4c383a8d3ab1a43137458a6a6fbe43cc32b01536
kishor-karthik-dev <18mech094@gmail.com>
2024-02-02 02:03:59 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import{useState}from "react";
 
 function App() {
+  const[calculator,setCalculator]=useState("")
+
+  const cancel=(a)=>{
+setCalculator("")
+  }
+  const result=()=>{
+    setCalculator(eval(calculator))
+  }
+  const all=(c)=>{
+    const lastIndex = calculator[calculator.length-1]
+    const symbols = ["+","-","*","/","+/-","%"]
+    if ((symbols.includes(lastIndex) && !symbols.includes(c))  || !symbols.includes(lastIndex) ){
+
+      setCalculator( calculator+c)
+    }
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <h3 className='header'>CALCULATOR</h3>
+      <p className='display'>{ calculator}</p>
+      </div>
+      <div>
+        <button className='buttonsS' onClick={(a)=>cancel("")}>AC</button>
+        <button className='buttonsS' onClick={()=>all("+/-")}>+/-</button>
+        <button className='buttonsS' onClick={()=>all("%")}>%</button>
+        <button className='buttonsS' onClick={()=>all("/")}>/</button>
+
+      </div>
+      <div>
+      <button className='buttons' onClick={()=>all("7")}>7</button>
+      <button className='buttons' onClick={()=>all("8")}>8</button>
+      <button className='buttons' onClick={()=>all("9")}>9</button>
+      <button className='buttonsS' onClick={()=>all("*")}>*</button>
+
+      </div>
+        <button className='buttons' onClick={()=>all("4")}>4</button>
+        <button className='buttons' onClick={()=>all("5")}>5</button>
+        <button className='buttons' onClick={()=>all("6")}>6</button>
+        <button className='buttonsS' onClick={()=>all("-")}>-</button>
+      <div>
+      <button className='buttons' onClick={()=>all("1")}>1</button>
+      <button className='buttons' onClick={()=>all("2")}>2</button>
+      <button className='buttons' onClick={()=>all("3")}>3</button>
+      <button className='buttonsS' onClick={()=>all("+")}>+</button>
+
+      </div>
+      <div>
+      <button className='buttonss' onClick={()=>all("0")}>0</button>
+      <button className='buttons' onClick={()=>all(".")}>.</button>
+      <button className='buttonsS' onClick={result}>=</button>
+
+      </div>
+
+
     </div>
   );
 }
